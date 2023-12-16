040b016e371777ddd1f414590c65cce795c15d2d
j.c.stringer21@gmail.com <j.c.stringer21@gmail.com>
2023-12-16 23:43:09 +0000

    started transforming the data in staging layer. preparing it for mart layer

--- models/staging/stg_globepay__acceptance.sql
+++ models/staging/stg_globepay__acceptance.sql
@@ -14,16 +14,18 @@ acceptance_transformation AS (
 
     SELECT
 
-        external_ref AS payment_id --renaming to payment_id. I believe the name "external_ref" should be given to the field "ref."
-        ,ref AS external_ref --in the api documentation provided we are given an example of an external_ref which matches the ref field in the data file. ref is not referred to anywhere in the documentation.
-        ,status payment_source --nowhere in the documentation do we get told what status means + it's true for every line. Not sure of it's purpose in this data. Perhaps useful outside demo/test purposes.
+        external_ref AS payment_id --renaming to payment_id. The column name "external_ref" should be given to the column "ref."
+        ,ref AS external_ref --in the api documentation provided we are given an example of an external_ref which matches the ref column data in the data file. ref is not referred to anywhere in the documentation.
+        ,status payment_source --nowhere in the documentation do we get told what status means + it's the only value is 'True'. Not sure of it's purpose in this data. Perhaps useful outside demo/test purposes.
         ,source AS payment_source --as we are only dealing with globepay data, globepay is our only source. This identifier would be useful if we had other sources.
         ,state AS payment_state
         ,cvv_provided AS payment_cvv_provided
         ,country AS payment_country_code
         ,currency AS payment_currency_code
         ,amount AS payment_amount_usd
-        -- extract the exchange rate from the rates column and convert the amount field into it's local amount
+        -- extract the exchange rate from the rates column and convert the amount field into it's local amount. rounding to 2 decimal places.
+        -- In a real scenario a seperate daily exchange rate table and macro would be useful in this situation, in a global company dealing with transaction/finances doing currency conversion is a very common task.
+        -- In this example if we got more currencies outside of the ones we have hard coded, the amount would not be converted.
         ,CASE
             WHEN currency = 'USD' THEN amount
             WHEN currency = 'CAD' THEN round(amount * CAST(JSON_EXTRACT_SCALAR(rates, '$.CAD') AS FLOAT64),2)
@@ -32,7 +34,7 @@ acceptance_transformation AS (
             WHEN currency = 'SGD' THEN round(amount * CAST(JSON_EXTRACT_SCALAR(rates, '$.SGD') AS FLOAT64),2)
             WHEN currency = 'AUD' THEN round(amount * CAST(JSON_EXTRACT_SCALAR(rates, '$.AUD') AS FLOAT64),2)
             WHEN currency = 'GBP' THEN round(amount * CAST(JSON_EXTRACT_SCALAR(rates, '$.GBP') AS FLOAT64),2)
-        ELSE 0  -- Handle other cases or currencies if necessary
+        ELSE amount  
         END AS payment_local_amount
         ,date_time
         -- Dates
@@ -43,8 +45,6 @@ acceptance_transformation AS (
     FROM
 
         source_globepay_Acceptance pd
-
-    order by date_time
 
 )
 
